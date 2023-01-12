54b903e4db2dd38a6e24ab3b4608328cbd3aea3a
Mohamed Hassan <mohamedhassan@Mohameds-Air.broadband>
2023-01-12 08:32:25 +0000

    amend pagespeed url

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -1,13 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { calculateAnnualEmissions, calculateAnnualEnergy, calculateEmissionsPerVisit, calculateEnergyConsumption, cupsOfCoffeeEquivalence } from '../../utils/calculations';
+import { calculateAnnualEmissions, calculateAnnualEnergy, calculateEnergyConsumption, cupsOfCoffeeEquivalence } from '../../utils/calculations';
    
   export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
   ) {
     try {
+      console.log(process.env.GOOGLE_API_KEY)
       const parsedUrl = req.body
-      const url = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${parsedUrl}&key=${process.env.GOOGLE_API_KEY}`;
+      const url = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${parsedUrl}&strategy=desktop&key=${process.env.GOOGLE_API_KEY}`;
       const resp = await fetch(url);
       const data = await resp.json()
       const pageSize = data.lighthouseResult.audits['total-byte-weight'].numericValue
@@ -17,6 +18,7 @@ import { calculateAnnualEmissions, calculateAnnualEnergy, calculateEmissionsPerV
       const cupsOfCoffee = cupsOfCoffeeEquivalence(annualEnergy.result)
       res.status(200).json({ data: { annualEmissions, annualEnergy, cupsOfCoffee}})
     }catch(e) {
+      console.log(e.message)
       res.status(404).json({ error: `${e.message}`})
     }
   }
