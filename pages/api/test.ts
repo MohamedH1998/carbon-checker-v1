ef5525732f824800b91c1c76f0448ef5b950dfd6
Mohamed Hassan <mohamedhassan@Mohameds-Air.broadband>
2023-01-13 19:06:24 +0000

    update api

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -7,7 +7,7 @@ import { calculateAnnualEmissions, calculateAnnualEnergy, calculateEnergyConsump
   ) {
     try {
       const parsedUrl = req.body
-      const url = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${parsedUrl}&strategy=desktop&key=${process.env.GOOGLE_API_KEY}`;
+      const url = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${parsedUrl}&fields=lighthouseResult(audits)&category=performance&strategy=desktop&key=${process.env.GOOGLE_API_KEY}`;
       const resp = await fetch(url);
       const data = await resp.json()
       const pageSize = data.lighthouseResult.audits['total-byte-weight'].numericValue
@@ -17,7 +17,6 @@ import { calculateAnnualEmissions, calculateAnnualEnergy, calculateEnergyConsump
       const cupsOfCoffee = cupsOfCoffeeEquivalence(annualEnergy.result)
       res.status(200).json({ data: { annualEmissions, annualEnergy, cupsOfCoffee}})
     }catch(e) {
-      console.log(e.message)
       res.status(404).json({ error: `${e.message}`})
     }
   }
